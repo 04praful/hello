be0772344c1753bc2263a1315c4baaf7764a67bc
04praful <126102812+04praful@users.noreply.github.com>
2023-02-24 13:38:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// items[2].style.backgroundColor = 'green';
+for(var i = 0; i < items.length; i++)
+  {
+    items[i].style.fontWeight = 'bold';
+  }
+// var headerTitle = document.getElementById('header-title');
+// // headerTitle.textContent = 'hello'
+// console.log(headerTitle.textContent);
+// var li = document.getElementsByTagName('li')
+// console.log(li);
+// console.log(li[1]);
+// li[1].textContent = 'hi';
+// for(var i = 0; i < li.length; i++)
+//   {
+//     li[i].style.fontWeight = 'bold';
+//   }
